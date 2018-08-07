338539eaed9b2b6910db9d050b34b471cc444705
DongWook Lee <vztpv@naver.com>
2018-08-08 02:58:27 +0900

    Add files via upload

---
+++ source/main.cpp
@@ -0,0 +1,19 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#include "new_paradox_parser.h"
+
+
+int main(void)
+{
+	wiz::load_data::UserType global;
+
+	wiz::load_data::LoadData::LoadDataFromFile("input.eu4", global, 0, 0); // DONE - support 0, 0
+
+	wiz::load_data::LoadData::SaveWizDB(global, "ouput.eu4");
+
+	return 0;
+}
+
